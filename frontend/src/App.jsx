ae351e2ffefa5ed1decb4f76646c76ea151df370
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-06-23 23:49:02 +0300

    Add the useColorMode to change the background of all application according to color mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
 import { Route, Routes } from "react-router"
 import Footer from "./components/Footer"
+import { useColorModeValue } from "./components/ui/color-mode"
+import { Box } from "@chakra-ui/react"
+
 
 function App() {
 
   return (
-    <>
+    <Box bg={useColorModeValue("gray.100", "gray.800")}>
     <NavBar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreatePage />} />
     </Routes>
     <Footer />
-    </>
+    </Box>
   )
 }
 
