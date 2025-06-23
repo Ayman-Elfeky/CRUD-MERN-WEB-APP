f6cc4e850521efcbbdb43fadea038751766812db
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-06-24 00:03:21 +0300

    Fix import Problems

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,5 @@
 import { useColorModeValue } from "@/components/ui/color-mode";
-import { Input, VStack } from "@chakra-ui/react";
+import { Input, VStack, Container, Heading, Box} from "@chakra-ui/react";
 import { useState } from "react";
 
 function CreatePage() {
@@ -17,25 +17,27 @@ function CreatePage() {
                     Create New Product
                 </Heading>
                 <Box
-                    w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
-                        <VStack spacing={4}>
-                            
-                            <Input 
+                    w={"full"}
+                    bg={useColorModeValue("white", "gray.800")}
+                    p={6}
+                    rounded={"lg"}
+                    shadow={"md"}
+                >
+                    <VStack spacing={4}>
+                        <Input
                             placeholder="Product Name"
                             name="Name"
                             value={newProduct.name}
-                            onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
-                            />
-
-                            <Input 
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+                        <Input
                             placeholder="Price"
                             name="price"
                             type="number"
                             value={newProduct.price}
-                            onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
-                            />
-
-                        </VStack>
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+                    </VStack>
                 </Box>
             </VStack>
         </Container>
