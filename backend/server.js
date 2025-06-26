ef4933f04feb9b3f79da61191b8ee2070ea53092
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-06-26 15:42:46 +0300

    Fix app error

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ import connectDB from './Config/db.js'
 import {  configDotenv  } from 'dotenv'
 import productsRouter from './routes/Product.route.js'
 
+const app = express();
+
 app.use(express.json());
 app.use('/api/products', productsRouter)
 configDotenv();
 const PORT = process.env.PORT;
 
-const app = express();
 
 
 app.listen(PORT, ()=> {
-    // connectDB();
+    connectDB();
     console.log("The server is started at http://localhost:" + PORT)
 })
