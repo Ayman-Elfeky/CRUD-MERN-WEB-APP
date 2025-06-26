ab6d9578a108f3508cded1c85aaf1b9d456c1d66
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-06-26 18:24:06 +0300

    update and fix productController

--- backend/controllers/Product.controllers.js
+++ backend/controllers/Product.controllers.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
 import Product from '../Models/Product.model.js'
 
-const getProducts = async (req, res)=> {
+const getProducts = async (req, res) => {
     try {
         const products = await Product.find({});
         console.log("Products has fetched successfully")
-        res.status(200).json({success: true, message: products})
+        res.status(200).json({ success: true, message: products })
     } catch (error) {
         console.log("Error in fetching Products: ", error.message)
-        res.status(500).json({success: false, message: error.message})
+        res.status(500).json({ success: false, message: error.message })
     }
 }
 
@@ -16,8 +16,8 @@ const createProduct = async (req, res) => {
     console.log(req.body);
     const product = req.body;
 
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success: false, message: 'Add all fields'})
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: 'Add all fields' })
     }
 
     const newProduct = new Product(product);
@@ -25,10 +25,10 @@ const createProduct = async (req, res) => {
     try {
         await newProduct.save();
         console.log("A new product has been added successfully:", newProduct)
-        res.status(201).json({success: true, message: `A new Product has added successfully`, data: newProduct })
+        res.status(201).json({ success: true, message: `A new Product has added successfully`, data: newProduct })
     } catch (error) {
         console.log("Error in add the product: ", error.message)
-        return res.status(500).json({success: false, message: 'Server Error'})
+        return res.status(500).json({ success: false, message: 'Server Error' })
     }
 }
 
@@ -37,19 +37,24 @@ const updateProduct = async (req, res) => {
     const product = req.body;
 
     // If the id is not found in mongoose
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        console.log('From Delete: The Product does not found');
-        return res.status(404).json({success: false, message: 'Product is Not Found'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        console.log('From Update: The Product does not found');
+        return res.status(404).json({ success: false, message: 'Product is Not Found' })
     }
 
     try {
         // {new:true} returns the product after updating but the opposite returns the product before updating
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
-        console.log('Product has been added successfully:', updatedProduct);
-        res.status(200).json({success: true, message: 'The product updated successfully'})
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+
+        if (!updatedProduct) {
+            return res.status(404).json({ success: false, message: 'Product not found' });
+        }
+
+        console.log('Product has been updated successfully:', updatedProduct);
+        res.status(200).json({ success: true, message: 'The product updated successfully', data: updatedProduct })
     } catch (error) {
         console.log('Error in updating a product: ', error.message);
-        res.status(500).json({success: false, message: 'Error in updating the product'})
+        res.status(500).json({ success: false, message: 'Error in updating the product' })
     }
 }
 
@@ -57,18 +62,18 @@ const deleteProduct = async (req, res) => {
     const { id } = req.params;
 
     // If the id is not found in mongoose
-    if(!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         console.log('The product does not found');
-        return res.status(404).json({successs: false, message: 'The product does not found'});
+        return res.status(404).json({ successs: false, message: 'The product does not found' });
     }
 
     try {
         const done = await Product.findByIdAndDelete(id);
         console.log("product has been deleted successfully:", done)
-        res.status(200).json({success: true, message: 'Product has deleted successfully'})
+        res.status(200).json({ success: true, message: 'Product has deleted successfully' })
     } catch (error) {
         console.log('Error cannot find the product: ', error.message);
-        res.status(500).json({success: false, message: "Couldn't find the product"})
+        res.status(500).json({ success: false, message: "Couldn't find the product" })
     }
 }
 
